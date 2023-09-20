4549320d6a1a367a161943a2f8bb25b4d3a4d737
Saeeda <saeedabatool39@gmail.com>
2023-09-20 19:59:29 -0400

    added a function to get inquiries

--- queries.js
+++ queries.js
@@ -44,3 +44,23 @@ module.exports = {
   addEmployee,
   updateEmployeeRole,
 };
+
+function start() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What would you like to do?',
+          choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit',
+          ],
+        },
+      ])
